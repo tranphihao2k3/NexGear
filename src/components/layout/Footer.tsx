df9ada1372252065faa85140090d02227cfd85c4
tranphihao2k3 <tranphihao2k3@gmail.com>
2026-04-12 21:57:14 +0700

    feat: implement Footer component with dynamic store information and add BCT certification assets

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -257,13 +257,20 @@ const Footer = () => {
                         >
                             {/* eslint-disable-next-line @next/next/no-img-element */}
                             <img
-                                src={s.bctType === 'registered'
-                                    ? "http://online.gov.vn/PublicImages/2015/08/27/11/20150827110756-dadangky.png"
-                                    : "http://online.gov.vn/PublicImages/2015/08/27/11/20150827110756-dathongbao.png"}
+                                src={s.bctType === 'registered' ? "/bct-registered.png" : "/bct-notified.png"}
                                 alt={s.bctType === 'registered' ? "Đã đăng ký Bộ Công Thương" : "Đã thông báo Bộ Công Thương"}
                                 width={180}
                                 height={68}
                                 style={{ objectFit: 'contain' }}
+                                onError={(e) => {
+                                    const img = e.target as HTMLImageElement;
+                                    if (!img.dataset.fallback) {
+                                        img.dataset.fallback = '1';
+                                        img.src = s.bctType === 'registered'
+                                            ? 'http://online.gov.vn/PublicImages/2015/08/27/11/20150827110756-dadangky.png'
+                                            : 'http://online.gov.vn/PublicImages/2015/08/27/11/20150827110756-dathongbao.png';
+                                    }
+                                }}
                             />
                         </a>
                     )}
